17d8f91384ff8dce161d62261ee0542721271e0c
pallav agarwal <pallavagarwal1997.pa75@gmail.com>
2020-11-10 17:49:13 +0530

    Update urls.py

--- almabase_api/urls.py
+++ almabase_api/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import commitDetails
 urlpatterns = [
+    # endpoint for rest api
     path('commit/<str:organization>/<int:countOfRepos>/<int:countOfCommitees>/', commitDetails, name="number_of_commits")
 ]
